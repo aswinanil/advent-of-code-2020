2ab848593d1c5d027a55a71a6f62dcd5f565e684
Aswin Anil <mraswinanil@gmail.com>
2020-12-15 19:24:46 +0800

    add solution for day 1, part 2

--- day_1.py
+++ day_1.py
@@ -2,7 +2,15 @@ file_handle = open('day_1_input.txt')
 expenses_string = file_handle.readlines()
 expenses = [int(i) for i in expenses_string]
 
-for i in range(0, len(expenses)-1):
-    for j in range(i+1, len(expenses)-1):
+last_index = len(expenses) - 1
+
+# part 1
+for i in range(0, last_index):
+    for j in range(i+1, last_index):
         if (expenses[i] + expenses[j] == 2020):
             print(expenses[i] * expenses[j])
+
+        # part 2
+        for k in range(j+1, last_index):
+            if (expenses[i] + expenses[j] + expenses[k] == 2020):
+                print(expenses[i] * expenses[j] * expenses[k])
