cf5248bcf900b8ec60ff7fe0aebb058ae3832ce1
Aswin Anil <mraswinanil@gmail.com>
2021-01-14 21:44:26 +0800

    add solution for day 10, part 2

--- day_10.py
+++ day_10.py
@@ -1,8 +1,8 @@
-# file_handle = open('input_day_10_example_2.txt')
 file_handle = open('input_day_10.txt')
 jolts = [int(line) for line in file_handle.read().splitlines()]
-
 jolts.sort()
+
+#  part 1
 count_1 = 0
 count_3 = 0
 
@@ -20,3 +20,53 @@ for i in range (len(jolts)-1):
 
 count_3 += 1
 print(count_1 * count_3)
+
+
+# Part 2
+class Node(object):
+    def __init__(self, value, index):
+        self.value = value
+        self.index = index
+        self.children = []
+
+    def add_child(self, obj):
+        self.children.append(obj)
+
+adapter_compatible_jolts_hash = {}
+def get_compatible_jolts(adapter):
+    if adapter.index in adapter_compatible_jolts_hash:
+        return adapter_compatible_jolts_hash[adapter.index]
+
+    compatible_jolts = []
+    i = adapter.index + 1
+
+    while i < len(jolts) and jolts[i] - adapter.value < 4:
+        compatible_jolts.append((jolts[i], i))
+        i += 1
+
+    adapter_compatible_jolts_hash[adapter.index] = compatible_jolts
+    return compatible_jolts
+
+distinct_ways_count = 0
+distinct_ways_hash= {}
+def populate_children(parent):
+    global distinct_ways_count
+
+    compatible_jolts = get_compatible_jolts(parent)
+    for (jolt, index) in compatible_jolts:
+        parent.add_child(Node(jolt, index))
+
+    if len(parent.children) == 0:
+        distinct_ways_count += 1
+
+    if parent.index in distinct_ways_hash:
+        distinct_ways_count += distinct_ways_hash[parent.index]
+    else:
+        parent.starting_distinct_ways_count = distinct_ways_count
+        for child in parent.children:
+            populate_children(child)
+        distinct_ways_hash[parent.index] = distinct_ways_count - parent.starting_distinct_ways_count
+
+outlet = Node(0, -1)
+populate_children(outlet)
+print(distinct_ways_count)
