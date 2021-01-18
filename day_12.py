42c8c0b5914eb2cc176dd9a0437f120841750e01
Aswin Anil <mraswinanil@gmail.com>
2021-01-18 20:02:26 +0800

    add solution for day 12, part 2

--- day_12.py
+++ day_12.py
@@ -1,12 +1,5 @@
-import pdb
-
-# file_handle = open('input_day_12_example.txt')
 file_handle = open('input_day_12.txt')
 instructions = [(line[0], int(line[1:])) for line in file_handle.read().splitlines()]
-
-east = 0
-north = 0
-current_direction = 'E'
 
 angles = {
     'E': 90,
@@ -33,7 +26,17 @@ def set_new_direction(action, value):
     current_angle %= 360
     current_direction = directions[current_angle]
 
-# print('\tdir: ' + current_direction, 'east: ' + str(east), 'north: ' + str(north))
+def set_new_direction_waypoint(action, value):
+    global waypoint_east, waypoint_north
+
+    value %= 360
+    direction_multiplier = 1 if action == 'R' else -1
+
+    for i in range(0, value, 90):
+        temp = waypoint_north
+        waypoint_north = direction_multiplier * -waypoint_east
+        waypoint_east = direction_multiplier * temp
+
 def perform_action(action, value):
     global east, north
 
@@ -52,9 +55,43 @@ def perform_action(action, value):
     else:
         print('unexpected input')
 
-    # print(action, value, '\tdir: ' + current_direction, 'east: ' + str(east), 'north: ' + str(north))
+def perform_action_waypoint(action, value):
+    global east, north, waypoint_east, waypoint_north
+
+    if action == 'N':
+        waypoint_north += value
+    elif action == 'S':
+        waypoint_north -= value
+    elif action == 'E':
+        waypoint_east += value
+    elif action == 'W':
+        waypoint_east -= value
+    elif action == 'L' or action == 'R':
+        set_new_direction_waypoint(action, value)
+    elif action == 'F':
+        north += waypoint_north * value
+        east += waypoint_east * value
+    else:
+        print('unexpected input')
+
+# Part 1
+east = 0
+north = 0
+current_direction = 'E'
 
 for (action, value) in instructions:
     perform_action(action, value)
+
+print(abs(east) + abs(north))
+
+
+# Part 2
+east = 0
+north = 0
+waypoint_east = 10  # relative to ship
+waypoint_north = 1  # relative to ship
+
+for (action, value) in instructions:
+    perform_action_waypoint(action, value)
 
 print(abs(east) + abs(north))
