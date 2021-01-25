064e2ab8d21ad53ad4813ba7abc9dcb2fe617196
Aswin Anil <mraswinanil@gmail.com>
2021-01-25 22:58:14 +0800

    add solution for day 13, part 2

--- day_13.py
+++ day_13.py
@@ -1,11 +1,10 @@
-import pdb
-
-# file_handle = open('input_day_13_example.txt')
 file_handle = open('input_day_13.txt')
 [earliest_timing, bus_services] = file_handle.read().splitlines()
+
+# Part 1
 earliest_timing = int(earliest_timing)
-bus_services = list(filter(lambda a: a != 'x', bus_services.split(',')))
-bus_services = [int(bus_service) for bus_service in bus_services]
+bus_services_without_x = list(filter(lambda a: a != 'x', bus_services.split(',')))
+bus_services_without_x = [int(bus_service) for bus_service in bus_services_without_x]
 
 def get_bus_timing(bus_service):
     earliest_bus_iteration = earliest_timing / bus_service
@@ -15,7 +14,7 @@ def get_bus_timing(bus_service):
 
     return earliest_bus_iteration * bus_service
 
-bus_timings = [(bus_service, get_bus_timing(bus_service)) for bus_service in bus_services]
+bus_timings = [(bus_service, get_bus_timing(bus_service)) for bus_service in bus_services_without_x]
 
 [earliest_bus, earliest_bus_timing] = bus_timings[0]
 for i in range(1, len(bus_timings)):
@@ -25,3 +24,51 @@ for i in range(1, len(bus_timings)):
         earliest_bus = bus
 
 print((earliest_bus_timing - earliest_timing) * earliest_bus)
+
+
+# Part 2
+bus_services = bus_services.split(',')
+first_bus_interval = int(bus_services[0])
+first_bus_timing = 0
+
+first_multiplier = 0
+multiplier_interval = 1
+highest_pattern_count = 0
+first_multiplier_dict = {}
+multiplier_interval_dict = {}
+
+pattern_found = False
+while not pattern_found:
+    first_bus_timing += first_bus_interval * multiplier_interval
+
+    next_timing = first_bus_timing
+    pattern_found = True
+    pattern_count = 0
+
+    for bus in bus_services[1:]:
+        next_timing += 1
+
+        if bus == 'x':
+            continue
+        else:
+            bus = int(bus)
+            if next_timing % bus == 0:
+                pattern_count += 1
+
+                if pattern_count > highest_pattern_count:
+                    highest_pattern_count = pattern_count
+
+                    if not pattern_count in first_multiplier_dict:
+                        first_multiplier_dict[pattern_count] = first_bus_timing / first_bus_interval
+
+                elif pattern_count == highest_pattern_count and pattern_count not in multiplier_interval_dict:
+                    multiplier_interval = first_bus_timing / first_bus_interval - first_multiplier_dict[pattern_count]
+                    multiplier_interval_dict[pattern_count] = multiplier_interval
+
+                continue
+            else:
+                pattern_found = False
+                break
+
+    if pattern_found:
+        print(first_bus_timing)
